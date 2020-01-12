88ab42282ccb5cef533448cfcd04be2555c68897
Marc Fortes <marcfortesbcn@gmail.com>
2020-01-12 20:41:02 +0100

    Login changes

--- login.php
+++ login.php
@@ -7,9 +7,6 @@
 <!--Header-->
 <?php
     require_once("plantillas/headerUnlogged.php");
-    
-    require_once("modelos/Connection.php");
-    require_once("modelos/UsersManagament.php");
 ?>
 
 <div class="container" id="login-container">
@@ -18,7 +15,7 @@
   </div>
   <div class="row">
     <div class="col-md-12" id="col-signup">
-      <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+      <form action="controladores/validateUserController.php" method="post">
         <div class="form-group">
           <label><b>Nombre de usuario o correo electrónico:</b></label>
           <input class="form-control" type="text" name="txtUsername">
@@ -31,25 +28,6 @@
           <input type="submit" class="btn btn-primary" style="align:center" name="btnEnviar" value="Entrar">
         </div>
       </form>
-      <?php
-
-        if (isset($_POST["btnEnviar"])) {
-          $nick_email = $_POST["txtUsername"];
-          $pass = $_POST["txtPassword"];
-
-          $conn = new Connection();
-          if (UsersManagament::validateUser($conn,$nick_email,$pass) == true) {
-            session_start();
-            $_SESSION["user"] = $nick_email;
-            header("Location:home.php");
-          }else {
-            echo "<div class='alert alert-danger' role='alert'>
-                    Nombre de usuario o contraseña incorrecto/a.
-                  </div>";
-          }
-        }
-
-      ?>
     </div>
   </div>
 </div>
